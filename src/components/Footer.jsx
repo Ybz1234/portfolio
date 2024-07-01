4ed0ea8d1441900cdb8703cb4ec0fc095492139e
Ybz1234 <122832333+Ybz1234@users.noreply.github.com>
2024-07-01 14:43:39 +0300

    01/07/2024
    Logo footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <footer className='bg-slate-800 text-white lg:px-48 px-4 py-20'>
             <div className='container mx-auto grid grid-cols-1 md:grid-cols-4 gap-4'>
                 <div className='mb-4 md:mb-0'>
-                    <span className='text-3xl font-semibold text-fuchsia-800 py-2 uppercase'><img src="..\public\newLogo.png" alt="" srcset=""/></span>
+                    <span className='text-3xl font-semibold text-fuchsia-800 py-2 uppercase'><img src="../newLogo.png" alt="" srcset=""/></span>
                 </div>
                 <div>
                     <h2 className='text-[22px] font-semibold text-fuchsia-800 py-2 uppercase'>Services</h2>
